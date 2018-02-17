6a623b8b2e2c8407750ecb9803f37ebee4860226
xoloitzcuintle <ozuniga.anz@gmail.com>
2018-02-16 18:59:31 -0600

    double delays

---
+++ TM4C/Labs/Lab_02/Project/Debug/main.d
@@ -0,0 +1,27 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.7.LTS/include/stdint.h
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.7.LTS/include/stdbool.h
+main.obj: C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/inc/hw_types.h
+main.obj: C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/inc/hw_memmap.h
+main.obj: C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/sysctl.h
+main.obj: C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/gpio.h
+main.obj: C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/inc/hw_ints.h
+main.obj: C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/interrupt.h
+main.obj: C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/timer.h
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.7.LTS/include/time.h
+main.obj: C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.7.LTS/include/linkage.h
+
+../main.c: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.7.LTS/include/stdint.h: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.7.LTS/include/stdbool.h: 
+C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/inc/hw_types.h: 
+C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/inc/hw_memmap.h: 
+C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/sysctl.h: 
+C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/gpio.h: 
+C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/inc/hw_ints.h: 
+C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/interrupt.h: 
+C:/TI/tirtos_tivac_2_16_01_14/products/TivaWare_C_Series-2.1.1.71b/driverlib/timer.h: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.7.LTS/include/time.h: 
+C:/TI/ccsv6/tools/compiler/ti-cgt-arm_15.12.7.LTS/include/linkage.h: 
